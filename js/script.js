6fae4c54443ecf2c7368104985b028af2d5e94e5
Yen Ko Romero Lay <yenkosac@gmail.com>
2021-02-07 20:10:02 -0500

    mas fotos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,59 @@
-var nombre = "yenko";
+// var nombre = prompt("pon tu nombre")
+// var nombre = confirm("entonces si eres?")
+// alert("nombre");
+
+// function calcularemosTUedad(numero1, numero2) {
+
+//     var tuedad = numero1 + numero2;
+//     alert(tuedad);
+//     return tuedad;
+// }
+
+// var estaEStuEdad = calcularemosTUedad(1, 4);
+// console.log(estaEStuEdad);
+
+
+
+// function quiensoy(nombre, apellido) {
+
+//     var nombre = prompt("dame tu nombre")
+//     var apellido = prompt("dame tu apellido")
+
+//     if (nombre === "yenko" && apellido === romero) {
+//         console.log("hola yenko")
+//     } else if (nombre === "jean" && apellido === romero) {
+//         console.log("hola jean")
+//     } else {
+//         console.log("entonces quien eres?")
+//     }
+// }
+
+// quiensoy(nombre)
+
+// var dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+
+// for (var a = 0; a < dias.length; a++) {
+//     if (dias === "martes") {
+//         console.log("este es martes");
+
+//     }
+//     // console.log(dias[a]);
+//     // alert(dias[dias]);
+
+// }
+
+
+function suma(numero1, numero2) {
+    var resultado = numero1 + numero2;
+    if (resultado > 10) {
+        alert("el resultado es " + resultado);
+    } else {
+        alert("mayor a 10, es " + resultado);
+    }
+}
+
+
+var numero1 = 12
+var numero2 = 5
+
+suma(numero1, numero2);
